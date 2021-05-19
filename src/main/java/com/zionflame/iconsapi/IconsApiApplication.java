f74a7dd4c6a4328d3ed94e46221cb8c9176d1b83
jeferson-menezes <jefersonjesusmenezes@yahoo.com.br>
2021-05-19 14:28:02 -0300

    implantação no servidor

--- src/main/java/com/zionflame/iconsapi/IconsApiApplication.java
+++ src/main/java/com/zionflame/iconsapi/IconsApiApplication.java
@@ -19,7 +19,7 @@ public class IconsApiApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**").allowedMethods("*")
-						.allowedOrigins("http://192.168.15.100:8080");
+				.allowedOrigins("http://localhost:8080", "http://192.168.15.100:8080");
 			}
 		};
 	}
